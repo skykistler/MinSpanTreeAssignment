48fc120d7d46868996a951a624262927fdac8517
Sky Kistler <skyler.kistler14@ncf.edu>
2015-05-05 18:41:06 -0400

    only needs to test if point in circumcircle

--- src/d_minSpanTree/controller/operation/algorithm/DelaunayTriangulation.java
+++ src/d_minSpanTree/controller/operation/algorithm/DelaunayTriangulation.java
@@ -1,6 +1,7 @@
 package d_minSpanTree.controller.operation.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import d_minSpanTree.model.Edge;
 import d_minSpanTree.model.GraphModelInterface;
@@ -44,28 +45,62 @@ public class DelaunayTriangulation implements GraphAlgorithm {
 		triangulation.add(super_triangle);
 
 		for (Vertex v : gmi.getVertices()) {
-			// find all bad triangles (point is inside circumcircle of triangle)
-			// add all edges to polygon that aren't shared by other bad
-			// triangles
-			// remove all bad triangles from triangulation
-			// add polygon triangles to triangulation
+			ArrayList<Triangle> bad_triangles = new ArrayList<Triangle>();
+			ArrayList<Edge> polygon = new ArrayList<Edge>();
+
+			for (Triangle t : triangulation)
+				if (inCircumcircle(t, v)) {
+					bad_triangles.add(t);
+
+					ArrayList<Edge> shared = new ArrayList<Edge>();
+					for (Edge e : polygon) {
+						for (int i = 0; i < 3; i++)
+							if (e.compare(t.edges[i])) {
+								shared.add(e);
+								shared.add(t.edges[i]);
+							}
+					}
+					polygon.addAll(Arrays.asList(t.edges));
+					polygon.removeAll(shared);
+				}
+
+			triangulation.removeAll(bad_triangles);
+
+			for (Edge e : polygon) {
+				Triangle new_triangle = new Triangle();
+				new_triangle.setVertex(0, e.getStart());
+				new_triangle.setVertex(1, e.getEnd());
+				new_triangle.setVertex(2, v);
+				new_triangle.makeEdges();
+				if (new_triangle.clockwise())
+					new_triangle.flipOrder();
+				triangulation.add(new_triangle);
+			}
 		}
 
-		// remove triangles that contain super-triangle vertex
+		ArrayList<Triangle> super_triangles = new ArrayList<Triangle>();
+		for (Triangle t : triangulation)
+			for (Vertex v : t.vertices)
+				if (v.getName().equals("super"))
+					super_triangles.add(t);
+		triangulation.removeAll(super_triangles);
 
 		for (int i = 0; i < triangulation.size(); i++) {
-			Vertex v0 = triangulation.get(i).getVertex(0);
-			Vertex v1 = triangulation.get(i).getVertex(0);
-			Edge e = new Edge(v0, v1);
-			double dx = v1.getX() - v0.getX();
-			double dy = v1.getY() - v0.getY();
-			e.setWeight(dx * dx + dy * dy);
-			gmi.getEdges().add(e);
+			for (Edge e : triangulation.get(i).edges) {
+				double dx = e.getEnd().getX() - e.getStart().getX();
+				double dy = e.getEnd().getY() - e.getStart().getY();
+				e.setWeight(dx * dx + dy * dy);
+				gmi.getEdges().add(e);
+			}
 		}
 
 		long endTime = System.nanoTime(); // Finish the total timing
 		float timeElapsed = (endTime - startTime) / 1000000.0f; // milliseconds
 		System.out.println("Edge creation O(f(nE,nV)???):" + timeElapsed);
+	}
+
+	public boolean inCircumcircle(Triangle t, Vertex v) {
+		return false;
 	}
 
 	public String getName() {
@@ -78,6 +113,8 @@ public class DelaunayTriangulation implements GraphAlgorithm {
 
 	class Triangle {
 		public Vertex[] vertices = new Vertex[3];
+		public Edge[] edges = new Edge[3];
+		public boolean[] shared_edges = new boolean[3];
 
 		public void setVertex(int i, Vertex v) {
 			vertices[i] = v;
@@ -85,6 +122,26 @@ public class DelaunayTriangulation implements GraphAlgorithm {
 
 		public Vertex getVertex(int i) {
 			return vertices[i];
+		}
+
+		public void makeEdges() {
+			edges[0] = new Edge(vertices[0], vertices[1]);
+			edges[1] = new Edge(vertices[1], vertices[2]);
+			edges[2] = new Edge(vertices[2], vertices[0]);
+		}
+
+		public void flipOrder() {
+			Vertex v0 = vertices[0];
+			Vertex v1 = vertices[1];
+			vertices[1] = v0;
+			vertices[0] = v1;
+		}
+
+		public boolean clockwise() {
+			double i = (vertices[1].getX() - vertices[0].getX()) * (vertices[1].getY() + vertices[0].getY());
+			i += (vertices[2].getX() - vertices[1].getX()) * (vertices[2].getY() + vertices[1].getY());
+			i += (vertices[0].getX() - vertices[2].getX()) * (vertices[0].getY() + vertices[2].getY());
+			return i > 0;
 		}
 	}
 
